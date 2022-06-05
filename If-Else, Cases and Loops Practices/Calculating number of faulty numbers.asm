2c1552dc446e11cbd390d26c1db3b33f88f21c3d
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2022-06-05 19:41:58 +0600

    A2 online solution

--- If-Else, Cases and Loops Practices/Calculating number of faulty numbers.asm
+++ If-Else, Cases and Loops Practices/Calculating number of faulty numbers.asm
@@ -5,6 +5,9 @@
     LF EQU 0AH  
     I DW ?
     FLAG DB 0
+    N DW ?
+    COUNT DW 0
+    SUM DW ?
     NEWLINE DB CR, LF, '$' 
 .CODE
     MAIN PROC  
@@ -12,6 +15,29 @@
         MOV AX, @DATA
         MOV DS, AX
         
+        CALL INPUT_INTEGER
+        MOV N, DX           ;Take input and store in N
+
+        CALL PRINT_NEWLINE
+
+        MOV AX, N
+        MOV COUNT, 0
+        
+        COUNT_FAULTY_WHILE:
+            CALL IS_FAULTY
+            CMP BX, 1       ;If it is not faulty, do nothing
+            JNE NOT_FAULTY
+            ;else, increment count
+            INC COUNT
+            
+            NOT_FAULTY:
+            DEC AX
+            CMP AX, 1
+            JG COUNT_FAULTY_WHILE
+        END_COUNT_FAULTY_WHILE:
+
+        MOV BX, COUNT
+        CALL PRINT_INTEGER
         
         END:
         ;RETURN 0
@@ -25,6 +51,41 @@
         INT 21H
         RET
     PRINT_NEWLINE ENDP
+
+    ;Checks if the number stored in AX is faulty or not
+    ;Saves 1 or 0 in BX
+    IS_FAULTY PROC
+        MOV SUM, 0
+        MOV BX, AX
+        DEC BX
+        LOOP_IS_FAULTY:
+            PUSH AX
+
+            XOR DX, DX  ;DX = 0
+            DIV BX      ;Divide AX by BX
+            CMP DX, 0   ;If DX is not equal to 0, then the number is not divisible by BX
+            JNE NOT_DIVISIBLE
+            ;else, the number is divisible by BX
+            ADD SUM, BX ;Add BX to SUM
+
+            NOT_DIVISIBLE:
+            POP AX
+
+            DEC BX
+            CMP BX, 1
+            JG LOOP_IS_FAULTY
+        END_LOOP_IS_FAULTY:
+        ;if SUM is greater than N, then the number is faulty
+        CMP SUM, AX
+        JG IS_FAULTY_TRUE
+        ;else, the number is not faulty
+        MOV BX, 0
+        RET 
+        
+        IS_FAULTY_TRUE:
+        MOV BX, 1
+        RET
+    IS_FAULTY ENDP
     
     ;Function that takes an integer as an input
     ;and saves it in DX
@@ -70,9 +131,8 @@
         RET
     INPUT_INTEGER ENDP
 
-    ;Prints the number stored in BX
     PRINT_INTEGER PROC
-        ;if(BX < -1) then the number is positive
+        ;if(BX >= 0) then the number is positive
         CMP BX, 0
         JGE POSITIVE        
         ;else, the number is negative
--- README.md
+++ README.md
@@ -28,19 +28,36 @@
                     <li>Go to Step 1.</li>
                     <li>End</li>
                 </ol>  
+                <br>
+                <b>Note:</b> You can assume any form of number to be in the range of signed 16 bit register.
             </p>
         </details>
     </div>
 - [Solution](https://github.com/fardinanam/Assembly-8086-Practice-Codes/blob/master/Insertion%20Sort%20and%20Binary%20Search/Insertion%20sort%20and%20binary%20search.asm)
 
-## Convert (3-9) based numbers to binary
+## Convert any based number to binary
 -   <div>
         <summery>
             Problem Specifications
         </summery>
         <details>
             <p>
-                Take a base value from (3 to 9) as input from the user. Then take an input N in the base value of given base. Convert the given number to binary and display.  
+                Take a base value as input from the user. Then take an input N in the base value of given base. Convert the given number to binary and display. <br>
+                <b>Note:</b> You can assume any form of number to be in the range of signed 16 bit register. 
+            </p>
+        </details>
+    </div>
+- [Solution](https://github.com/fardinanam/Assembly-8086-Practice-Codes/blob/master/If-Else%2C%20Cases%20and%20Loops%20Practices/Convert%20any%20based%20numbers%20to%20binary.asm)
+
+## Calculating number of faulty numbers
+-   <div>
+        <summery>
+            Problem Specifications
+        </summery>
+        <details>
+            <p>
+                A <i>faulty</i> number is one for which the sum of its proper divisors is greater than the number itself. The proper divisors of a number are all the divisors except 1 and the number itself. For example, 12 is a faulty number because the sum of the proper divisors is 2+3+4+5=15 which is greater than 12. Write a program to take an integer number N as input and output the number of faulty numbers between 1 and N <br>
+                <b>Note:</b> You can assume any form of number to be in the range of signed 16 bit register. 
             </p>
         </details>
     </div>
